c26ef368a9014b186d573c1d08154a6cdf746b45
Boris Nikolic <borisnikolic2302@gmail.com>
2024-05-27 10:32:26 +0200

    Prisma init

--- components/shared/ThemeSwitcher.tsx
+++ components/shared/ThemeSwitcher.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import { IoIosMoon } from "react-icons/io";
-import { FaSun } from "react-icons/fa6";
+import { LuSunMedium } from "react-icons/lu";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
@@ -20,7 +20,7 @@ export function ModeToggle() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" size="icon">
-          <FaSun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <LuSunMedium className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <IoIosMoon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
