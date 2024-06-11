a47babd696970bc008322e4a337b6bd033fce9ed
Boris Nikolic <borisnikolic2302@gmail.com>
2024-06-11 19:59:15 +0200

    fixed Form display

--- components/shared/(improvements-components)/ImprovementDetails.tsx
+++ components/shared/(improvements-components)/ImprovementDetails.tsx
@@ -20,15 +20,22 @@ interface ResponseProps {
 }
 
 const FormDataDisplay = ({ formData }: { formData: FormValues }) => (
-  <div className="bg-gray-100 dark:bg-gray-900 flex-col max-h-96 overflow-y-scroll max-w-full w-full rounded-lg shadow-lg p-4 border border-gray-600 mt-4">
-    <h2 className="text-xl font-bold text-white mb-4">Provided Form Data</h2>
+  <div className="bg-gray-50 dark:bg-gray-800 flex-col max-h-96 overflow-y-scroll max-w-full w-full rounded-lg shadow-lg p-6 border border-gray-300 dark:border-gray-700 mt-4">
+    <h2 className="text-xl font-bold text-gray-900 dark:text-gray-50 mb-4">
+      Provided Form Data
+    </h2>
     <div className="space-y-2">
       {Object.entries(formData).map(([key, value]) => (
-        <div key={key} className="p-2 rounded-lg bg-gray-800">
-          <h3 className="text-sm font-semibold text-gray-300 capitalize">
+        <div
+          key={key}
+          className="p-4 rounded-lg bg-gray-100 dark:bg-gray-900 shadow-md"
+        >
+          <h3 className="text-sm font-semibold text-gray-700 dark:text-gray-300 capitalize">
             {key.replace(/([A-Z])/g, " $1")}:
           </h3>
-          <p className="text-gray-400 break-words text-sm">{value}</p>
+          <p className="text-gray-600 dark:text-gray-400 break-words text-sm">
+            {value}
+          </p>
         </div>
       ))}
     </div>
