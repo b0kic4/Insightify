4150c965802ce698a5aa9c466696aa279200133f
Boris Nikolic <borisnikolic2302@gmail.com>
2024-06-22 19:18:53 +0200

    return type in webhook fixed

--- app/api/squeezy-webhook/route.ts
+++ app/api/squeezy-webhook/route.ts
@@ -3,11 +3,9 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function POST(req: any) {
+export async function POST(req: any): Promise<void | Response> {
   console.log("WEBHOOK CALLED");
-  // NOTE: Go through the logs and configure the functionality
   try {
-    // Catch the event type
     const clonedReq = req.clone();
     const eventType = req.headers.get("X-Event-Name");
     const body = await req.json();
@@ -28,7 +26,6 @@ export async function POST(req: any) {
     console.log("event type: ", eventType);
     console.log("body of the request: ", body);
 
-    // Extract relevant data from the body
     const { data } = body;
     const { attributes } = data;
     const {
@@ -43,14 +40,10 @@ export async function POST(req: any) {
       card_last_four,
       card_brand,
       subscription_id,
-      total_formatted,
     } = attributes;
 
-    // Find the user by email
     const user = await prisma.user.findUnique({
-      where: {
-        email: user_email,
-      },
+      where: { email: user_email },
     });
 
     if (!user) {
@@ -64,7 +57,6 @@ export async function POST(req: any) {
       eventType === "subscription_created" ||
       eventType === "subscription_updated"
     ) {
-      // Create or update card details
       const card = await prisma.card.upsert({
         where: {
           lastFour_brand_userId: {
@@ -77,22 +69,15 @@ export async function POST(req: any) {
         create: {
           lastFour: card_last_four,
           brand: card_brand,
-          user: {
-            connect: {
-              id: user.id,
-            },
-          },
+          user: { connect: { id: user.id } },
         },
       });
 
       console.log(`Card created or updated for user ${user_email}`);
       console.log("data.id: ", data.id);
 
-      // Update the user's plan
       await prisma.plan.upsert({
-        where: {
-          variantId: variant_id,
-        },
+        where: { variantId: variant_id },
         update: {
           productId: product_id,
           productName: product_name,
@@ -107,75 +92,47 @@ export async function POST(req: any) {
           variantId: variant_id,
           subscriptionId: Number(data.id),
           name: product_name,
-          user: {
-            connect: {
-              id: user.id,
-            },
-          },
+          user: { connect: { id: user.id } },
           status: status,
           createdAt: new Date(created_at),
           renewsAt: new Date(renews_at),
           lastFour: card_last_four,
-          card: {
-            connect: {
-              id: card.id,
-            },
-          },
+          card: { connect: { id: card.id } },
         },
       });
 
       console.log(`Subscription ${status} for user ${user_email}`);
     } else if (eventType === "subscription_payment_success") {
-      let subtotal_formatted_string: string;
-      subtotal_formatted_string = String(subtotal_formatted);
-
-      console.log("subtotal_formatted_string: ", subtotal_formatted_string);
-
-      console.log("subscription_id: ", subscription_id);
-
       const foundPlan = await prisma.plan.findFirst({
-        where: {
-          subscriptionId: subscription_id,
-          userId: user.id,
-        },
+        where: { subscriptionId: subscription_id, userId: user.id },
       });
-
-      console.log("foundPlan: ", foundPlan);
 
       if (!foundPlan) {
         console.log("No found plan");
-        return;
+        return new Response(JSON.stringify({ message: "Plan not found" }), {
+          status: 404,
+        });
       }
 
-      const updatedPlan = await prisma.plan.update({
-        where: {
-          id: foundPlan.id,
-        },
-        data: {
-          price: subtotal_formatted_string,
-        },
+      await prisma.plan.update({
+        where: { id: foundPlan.id },
+        data: { price: String(subtotal_formatted) },
       });
-
-      console.log("updatedPlan: ", updatedPlan);
 
       console.log(
         `Plan price updated to ${subtotal_formatted} for user ${user_email}`,
       );
-
-      return updatedPlan;
     } else if (eventType === "subscription_cancelled") {
-      // Handle subscription cancellation
       await prisma.plan.deleteMany({
-        where: {
-          variantId: variant_id,
-          userId: user.id,
-        },
+        where: { variantId: variant_id, userId: user.id },
       });
 
       console.log(`Subscription cancelled for user ${user_email}`);
     }
 
-    return new Response(JSON.stringify({ message: "Webhook received" }));
+    return new Response(JSON.stringify({ message: "Webhook received" }), {
+      status: 200,
+    });
   } catch (err) {
     console.error(err);
     return new Response(JSON.stringify({ message: "Server error" }), {
